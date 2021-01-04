a46cb2f9b4fe4f7db47cd1655ab825bcc74225b7
Edgar Runnman <edgar.runnman@gmail.com>
2021-01-04 20:15:09 +0100

    bättre konroller till antal rättta, formatering

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 "use strict";
 const items = document.querySelectorAll('section > div');
 let catsCounter = 0;
-let nonCatsCounter = 0;
 
-for (var i = 0; i < items.length; i++) {
+for (var i = 0; i < items.length; i++)
+    if (items[i].classList.contains("katt"))
+        catsCounter++
+
+        let nonCatsCounter = 0;
+
+for (var i = 0; i < items.length; i++)
     items[i].addEventListener("click", function() {
         this.classList.toggle("vald");
         var isCat = this.classList.contains("katt");
         var isPicked = this.classList.contains("vald");
 
-        if (isCat & isPicked) {
-            catsCounter++;
-        } else if (isCat & !isPicked) {
-            catsCounter--;
-        } else if (!isCat & isPicked) {
-            nonCatsCounter++;
-        } else if (!isCat & !isPicked) {
-            nonCatsCounter--;
-        }
+        if (isCat & isPicked) catsCounter--;
+        else if (isCat & !isPicked) catsCounter++;
+        else if (!isCat & isPicked) nonCatsCounter++;
+        else if (!isCat & !isPicked) nonCatsCounter--;
 
-        if (catsCounter == 3 & nonCatsCounter == 0) {
+        if (catsCounter == 0 & nonCatsCounter == 0)
             document.querySelector('button').disabled = false;
-        } else {
+        else
             document.querySelector('button').disabled = true;
-        }
     });
-}
