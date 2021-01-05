992c682ed92b4b442d4b7408535a3c7cd751a5c3
Edgar Runnman <edgar.runnman@gmail.com>
2021-01-05 16:28:50 +0100

    små fix

--- script.js
+++ script.js
@@ -10,29 +10,24 @@ items.forEach(function(item) {
 
 function filter(array, test, invert = false) {
     let passed = [];
-    for (let element of array) {
-        debugger;
+    for (let element of array)
         if (!invert) {
-            if (test(element)) {
+            if (test(element))
                 passed.push(element);
-            }
         } else {
-            if (!test(element)) {
+            if (!test(element))
                 passed.push(element);
-
-            }
         }
-    }
     return passed;
 }
 
 function TryEnableSubmit() {
     var targetItems = Array.from(document.querySelectorAll(".katt"));
-    var chosentargetItems = filter(targetItems, item => item.classList.contains("vald"));
+    var chosenTargetItems = filter(targetItems, item => item.classList.contains("vald"));
     var failItems = filter(items, item => item.classList.contains("katt"), true)
     var chosenFailItems = filter(failItems, item => item.classList.contains("vald"));
 
-    if (chosentargetItems.length == targetItems.length && chosenFailItems == 0)
+    if (chosenTargetItems.length == targetItems.length && chosenFailItems == 0)
         document.querySelector('button').disabled = false;
     else
         document.querySelector('button').disabled = true;
