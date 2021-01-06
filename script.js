bb55dd6f21fd81b1ef56d543252b6ac06bb9f4f7
Edgar Runnman <edgar.runnman@gmail.com>
2021-01-06 10:55:01 +0100

    förenkling jag komm på medans jag sprang

--- script.js
+++ script.js
@@ -8,27 +8,13 @@ items.forEach(function(item) {
     })
 });
 
-function filter(array, test, invert = false) {
-    let passed = [];
-    for (let element of array)
-        if (!invert) {
-            if (test(element))
-                passed.push(element);
-        } else {
-            if (!test(element))
-                passed.push(element);
-        }
-    return passed;
-}
-
 function tryEnableSubmit() {
-    let targetItems = Array.from(document.querySelectorAll(".katt"));
-    let chosenTargetItems = filter(targetItems, item => item.classList.contains("vald"));
-    let failItems = filter(items, item => item.classList.contains("katt"), true)
-    let chosenFailItems = filter(failItems, item => item.classList.contains("vald"));
-
-    if (chosenTargetItems.length == targetItems.length && chosenFailItems == 0)
+    let test = equals(Array.from(document.querySelectorAll(".katt")).sort(),
+        Array.from(document.querySelectorAll(".vald")).sort());
+    if (test)
         document.querySelector('button').disabled = false;
     else
         document.querySelector('button').disabled = true;
 }
+
+const equals = (a, b) => a.every((x, y) => x === b[y]);
